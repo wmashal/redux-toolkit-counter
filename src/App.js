ea7f0ae6ce4968c561db26009a7ec92c82b6cafd
Wael Mashal <wael.mashal@sap.com>
2022-11-12 13:10:18 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector,useDispatch } from 'react-redux';
+import {actions} from './store/index'
 
 function App() {
+  const dispatcher = useDispatch();
+  const increment = () => {
+    dispatcher(actions.increment())
+  }
+
+  const decrement = () => {
+    dispatcher(actions.decrement())
+  }
+
+  const addBy = () => {
+    dispatcher(actions.addBy(10))
+  }
+
+  const counter = useSelector((state) => state.counter)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counter</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>decrement</button>
+      <button onClick={addBy}>decrement</button>
     </div>
   );
 }
